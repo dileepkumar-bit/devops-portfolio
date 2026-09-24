ca5fba880577ce0afe3d005d364b9105488b9d66
kalisettidileepkumar <dileeproyalk210@gmail.com>
2026-09-24 20:00:29 +0530

    Add system health check script
    This script checks the system's health by monitoring memory usage and disk space, and outputs the results along with the hostname and uptime.

--- 01-linux-shell-scripting/scripts/system_health.sh
+++ 01-linux-shell-scripting/scripts/system_health.sh
@@ -1,1 +1,40 @@
+#!/bin/bash
 
+##############################
+##############################
+#Author: Dileep
+#Script: System_Health
+##############################
+##############################
+echo "================================="
+echo "LINUX SYSTEM HEALTH CHECK"
+echo "================================="
+MEMORY_THRESHOLD=80
+DISK_THRESHOLD=80
+
+# hostname:
+echo ""
+host=$(hostname)
+echo "Hostname: $host"
+# print uptime:
+time=$(uptime | awk -F'up |,' '{print $2}')
+load=$(uptime | awk -F'load average: ' '{print $2}')
+echo "Uptime: $time"
+echo "CPU Load: $load"
+echo ""
+# memory usage:
+memory=$(free -h | awk '/Mem:/ {printf "%.0f", $3/$2*100}')
+echo "Memory usage: $memory%"
+# disk space:
+disk=$(df -h / | awk 'NR==2 {gsub("%","",$5); print $5}')
+echo "Disk Space: $disk%"
+echo ""
+if [ "$memory" -lt "$MEMORY_THRESHOLD" ] && [ "$disk" -lt "$DISK_THRESHOLD" ]; then
+    echo "========================"
+    echo "SYSTEM HEALTH: HEALTHY"
+    echo "========================"
+else
+    echo "========================="
+    echo "SYSTEM HEALTH: UNHEALTHY"
+    echo "========================="
+fi
