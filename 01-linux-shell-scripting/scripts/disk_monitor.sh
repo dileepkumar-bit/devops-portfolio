8606e73694785664dec084c73d64761348a66290
kalisettidileepkumar <dileeproyalk210@gmail.com>
2026-09-24 20:54:01 +0530

    Add disk space monitoring script

--- 01-linux-shell-scripting/scripts/disk_monitor.sh
+++ 01-linux-shell-scripting/scripts/disk_monitor.sh
@@ -1,1 +1,30 @@
-
+#!/bin/bash
+##############################
+##############################
+#Author: Dileep
+#Script: Disk Space Monitor
+##############################
+##############################
+echo "========================"
+echo "DISK SPACE MONITOR"
+echo "========================"
+echo ""
+DISK_THRESHOLD=80
+# Disk Filesystem:
+file=$(df -h / | awk 'NR==2 {print $6}')
+echo "Filesystem: $file"
+# Disk Usage:
+disk=$(df -h / | awk 'NR==2 {print $5}' | tr -d '%')
+echo "Disk Usage: $disk%"
+# Disk Threshold:
+echo "Threshold: $DISK_THRESHOLD%"
+echo ""
+if [ "$disk" -lt "$DISK_THRESHOLD" ]; then
+    echo "========================"
+    echo "DISK STATUS: HEALTHY"
+    echo "========================"
+else
+    echo "========================="
+    echo "DISK STATUS: WARNING"
+    echo "========================="
+fi
