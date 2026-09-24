1086e0b50e09fe1b9a3577fd8536a5deef6d033f
kalisettidileepkumar <dileeproyalk210@gmail.com>
2026-09-24 23:15:32 +0530

    Add service monitoring script

--- 01-linux-shell-scripting/scripts/service_monitor.sh
+++ 01-linux-shell-scripting/scripts/service_monitor.sh
@@ -1,1 +1,26 @@
-
+#!/bin/bash
+##############################
+##############################
+#Author: Dileep
+#Script: service_monitor
+##############################
+##############################
+echo "========================"
+echo "SERVICE MONITOR"
+echo "========================"
+echo ""
+SERVICE_NAME="$1"
+#Service Name:
+echo "Service: $SERVICE_NAME"
+#Service status:
+STATUS=$(systemctl is-active "$SERVICE_NAME")
+echo "status: $STATUS"
+if [ "$STATUS" = "active" ]; then
+    echo "======================="
+    echo "SERVICE STATUS: RUNNING"
+    echo "======================="
+else
+    echo "======================="
+    echo "SERVICE STATUS: STOPPED"
+    echo "======================="
+fi
