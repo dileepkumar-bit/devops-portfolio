355301bf7571b3eb2fe3458b5db6f947278730f8
kalisettidileepkumar <dileeproyalk210@gmail.com>
2026-09-25 00:02:16 +0530

    Add log monitoring script for system health check

--- 01-linux-shell-scripting/scripts/log_monitor.sh
+++ 01-linux-shell-scripting/scripts/log_monitor.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
 
+##############################
+##############################
+#Author: Dileep
+#Script: System_Health
+##############################
+##############################
+echo "================================="
+echo "LOG MONITOR"
+echo "================================="
+
+ERROR_COUNT=$(grep -ic "error" /var/log/syslog)
+#Log file:
+echo "Log File: /var/log/syslog"
+#Log error count:
+echo "ERROR Count: $ERROR_COUNT"
+
+if [ "$ERROR_COUNT" -eq 0 ]; then
+    echo "==================="
+    echo "LOG STATUS: HEALTHY"
+    echo "==================="
+else
+    echo "====================="
+    echo "LOG STATUS: WARNING"
+    echo "====================="
+fi
