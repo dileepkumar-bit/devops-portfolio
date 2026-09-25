1be51cd79612acf5010cb97009acef26b734e4f3
kalisettidileepkumar <dileeproyalk210@gmail.com>
2026-09-25 22:48:13 +0530

    Refactor system_health.sh for consistency and clarity

--- 01-linux-shell-scripting/scripts/system_health.sh
+++ 01-linux-shell-scripting/scripts/system_health.sh
@@ -2,33 +2,41 @@
 
 ##############################
 ##############################
-#Author: Dileep
-#Script: System_Health
+# Author: Dileep
+# Script: System_Health
 ##############################
 ##############################
-echo "================================="
+
+echo "=========================="
 echo "LINUX SYSTEM HEALTH CHECK"
-echo "================================="
-#Source
+echo "=========================="
+
+# central configuration file
 source "$(dirname "$0")/../config/monitor.conf"
-# hostname:
+
+# Hostname
 echo ""
-host=$(hostname)
-echo "Hostname: $host"
-# print uptime:
-time=$(uptime | awk -F'up |,' '{print $2}')
-load=$(uptime | awk -F'load average: ' '{print $2}')
-echo "Uptime: $time"
-echo "CPU Load: $load"
+HOST=$(hostname)
+echo "Hostname: $HOST"
+
+# Uptime
+TIME=$(uptime | awk -F'up |,' '{print $2}')
+LOAD=$(uptime | awk -F'load average: ' '{print $2}')
+echo "Uptime: $TIME"
+echo "CPU Load: $LOAD"
 echo ""
-# memory usage:
-memory=$(free -h | awk '/Mem:/ {printf "%.0f", $3/$2*100}')
-echo "Memory usage: $memory%"
-# disk space:
-disk=$(df -h / | awk 'NR==2 {gsub("%","",$5); print $5}')
-echo "Disk Space: $disk%"
+
+# Memory usage
+MEMORY=$(free -h | awk '/Mem:/ {printf "%.0f", $3/$2*100}')
+echo "Memory usage: $MEMORY%"
+
+# Disk space
+DISK=$(df -h / | awk 'NR==2 {gsub("%","",$5); print $5}')
+echo "Disk Space: $DISK%"
 echo ""
-if [ "$memory" -lt "$MEMORY_THRESHOLD" ] && [ "$disk" -lt "$DISK_THRESHOLD" ]; then
+
+# Check system_health
+if [ "$MEMORY" -lt "$MEMORY_THRESHOLD" ] && [ "$DISK" -lt "$DISK_THRESHOLD" ]; then
     echo "========================"
     echo "SYSTEM HEALTH: HEALTHY"
     echo "========================"
