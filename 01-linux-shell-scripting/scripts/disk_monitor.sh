8c59b23d8cd3cdf5ea9cac29584d26404711f2da
kalisettidileepkumar <dileeproyalk210@gmail.com>
2026-09-25 11:50:54 +0530

    Update comments for clarity in disk_monitor.sh

--- 01-linux-shell-scripting/scripts/disk_monitor.sh
+++ 01-linux-shell-scripting/scripts/disk_monitor.sh
@@ -10,16 +10,16 @@ echo "DISK SPACE MONITOR"
 echo "========================"
 echo ""
 DISK_THRESHOLD=80
-# Disk Filesystem:
+# Check Disk Filesystem:
 FILE=$(df -h / | awk 'NR==2 {print $6}')
 echo "Filesystem: $FILE"
-# Disk Usage:
+# Check Disk Usage:
 DISK=$(df -h / | awk 'NR==2 {print $5}' | tr -d '%')
 echo "Disk Usage: $DISK%"
 # Disk Threshold:
 echo "Threshold: $DISK_THRESHOLD%"
 echo ""
-#Disk Status:
+#Check Disk Status:
 if [ "$DISK" -lt "$DISK_THRESHOLD" ]; then
     echo "========================"
     echo "DISK STATUS: HEALTHY"
